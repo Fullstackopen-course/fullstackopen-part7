e387142c0ab3381548551773a826cf440098aa78
poliveir <pedrol2507@gmail.com>
2025-09-18 17:38:19 +0100

    7.3: Routed Anecdotes, step3

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Routes, Route, Link, useMatch } from 'react-router-dom'
+import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom'
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -100,6 +100,7 @@ const CreateNew = (props) => {
 }
 
 const App = () => {
+	const navigate = useNavigate()
   const [anecdotes, setAnecdotes] = useState([
     {
       content: 'If it hurts, do it more often',
@@ -126,6 +127,12 @@ const App = () => {
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
+
+	navigate('/')
+	setNotification(`a new anecdote ${anecdote.content} created!`)
+	setTimeout(() => {
+	  setNotification('')
+	}, 5000)
   }
 
   const anecdoteById = (id) =>
@@ -147,6 +154,11 @@ const App = () => {
 			<h1>Software anecdotes</h1>
 
 			<Menu />
+
+			{
+				notification &&
+				<p>{notification}</p>
+			}
 
 			<Routes>
 				<Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
